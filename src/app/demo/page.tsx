9bf109d2959ba87aab51dbf84ed810ac50d39900
smohen985-ux <smohen985@gmail.com>
2026-07-29 20:33:14 -0700

    Create page.tsx

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,9 @@
+import AetherFlowHeroDemo from "@/components/ui/aether-flow-hero-demo";
+
+export default function DemoPage() {
+  return (
+    <main className="App bg-black">
+      <AetherFlowHeroDemo />
+    </main>
+  );
+}
