d1c884e9a2194d9ebe44617b4e5d9a1ac1b1878b
smohen985-ux <smohen985@gmail.com>
2026-07-30 17:42:59 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,30 +6,30 @@ import Leadership from "@/components/Leadership";
 import Services from "@/components/Services";
 import HowWeWork from "@/components/HowWeWork";
 import WhoItsFor from "@/components/WhoItsFor";
+import Values from "@/components/Values";
 import Reviews from "@/components/Reviews";
-import FAQs from "@/components/FAQs";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 export default function Home() {
-return (
-<>
-<Nav />
-<main>
-<div id="home">
-<AetherFlowHero />
-</div>
-<ProofBand />
-<About />
-<Leadership />
-<Services />
-<HowWeWork />
-<WhoItsFor />
-<Reviews />
-<FAQs />
-<Contact />
-</main>
-<Footer />
-</>
-);
+  return (
+    <>
+      <Nav />
+      <main>
+        <div id="home">
+          <AetherFlowHero />
+        </div>
+        <ProofBand />
+        <About />
+        <Leadership />
+        <Services />
+        <HowWeWork />
+        <WhoItsFor />
+        <Values />
+        <Reviews />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
 }
